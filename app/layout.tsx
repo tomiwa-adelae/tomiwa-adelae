c847ff05bc2520c82abdab37d0457bf609719f47
Tomiwa Adelae <thetommedia@gmail.com>
2025-02-26 11:02:33 +0100

    Updated the SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 	description:
 		"Tomiwa Adelae, a skilled, result-driven full stack developer known for crafting immersive online experiences. This portfolio spans from corporate websites to dynamic e-commerce platforms. With a focus on user-centric design and innovative technologies, he brings ideas to life and empowers businesses to succeed in the digital space.",
 	keywords:
-		"Tomiwa, Adelae, Tomiwa Adelae, Adetomiwa, Adetomiwa Adelae, Portfolio, website, website developer, web, website development, programmer, programming, backend, frontend, full stack, stack",
+		"tomiwa, adelae, tomiwa adelae, adetomiwa, ademola, adetomiwa adelae, portfolio, website, website developer, web, website development, programmer, programming, backend, frontend, full stack, stack",
 	icons: {
 		icon: "/assets/favicon.ico",
 	},
